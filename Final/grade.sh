d528a659c66b6676c290193894dc052c6fca9be8
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-12-21 21:24:33 -0500

    Automatically assigning 0-values if haven't passed any values to the rubric

--- Final/grade.sh
+++ Final/grade.sh
@@ -6,22 +6,40 @@ csv_file="./grades.csv"
 single_student_actions() {
 	pdf="$1"
 	csv="$2"
+	page=3
 
 	student=$(echo "$pdf" | awk -F'.' '{print $1}')
-	zathura "$pdf"
-	read -p "PartII: Algorithm identified (out of 1)\n" algorithm
-	read -p "PartII: Physical problem identified (out of 1)\n" physics
-	read -p "PartII: Correctness of interpretation (out of 2)\n" correctness
-	read -p "PartII: Clarity (out of 1)\n" clarity
-	read -p "PartIV: Student chose Q3? (y or nothing)\n" part4
+	zathura "$pdf" -P "$page"
+	read -p "Part2: Algorithm identified (out of 1): " algorithm
+	read -p "Part2: Physical problem identified (out of 1): " physics
+	read -p "Part2: Correctness of interpretation (out of 2): " correctness
+	read -p "Part2: Clarity (out of 1): " clarity
+	read -p "Part3Q4: Student chose Q4? (y or nothing): " part3
 
-    string="$student,$algorithm,$physics,$correctness,$clarity,$part4"
+	if [[ "$algorithm" -eq "" ]]; then
+	    algorithm=0
+	fi
+	if [[ "$physics" -eq "" ]]; then
+	    physics=0
+	fi
+	if [[ "$correctness" -eq "" ]]; then
+	    correctness=0
+	fi
+	if [[ "$clarity" -eq "" ]]; then
+	    clarity=0
+	fi
+
+	if [[ "$part3" -eq "" ]]; then
+	    part3="n"
+	fi
+
+    string="$student,$algorithm,$physics,$correctness,$clarity,$part3"
 	echo "$string" >> "$csv"
 	echo "Printed $string into $csv"
 }
 
 loop_through_students() {
-	directory=$( realpath "$1" )
+	directory=$(realpath "$1" )
 	csv_file=$( realpath "$2" )
 	setup_csv_file "$csv_file"
 	(
@@ -36,7 +54,7 @@ loop_through_students() {
 
 setup_csv_file() { 
 	csv_file="$1"
-	csv_columns="Name,P2_Algorithm,P2_Physics,P2_Correctness,P2_Clarity"
+	csv_columns="Name,P2_Algorithm,P2_Physics,P2_Correctness,P2_Clarity,P3Q4"
 	echo "$csv_columns" > "$csv_file"
 }
 
